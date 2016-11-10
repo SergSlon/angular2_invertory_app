bac460428869ca8506a52dc849bb43a900f91440
SergSlon <liamin.web@gmail.com>
2016-11-11 00:23:19 +0200

    generate product-row component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ProductRowComponent } from './product-row/product-row.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductRowComponent
   ],
   imports: [
     BrowserModule,
