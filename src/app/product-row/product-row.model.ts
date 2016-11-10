99e456d21cdf7b003a2623125c9d63e07a512c8f
SergSlon <liamin.web@gmail.com>
2016-11-11 00:37:06 +0200

    add product-row model

---
+++ src/app/product-row/product-row.model.ts
@@ -0,0 +1,14 @@
+/**
+ * Provides a `Product` object
+ */
+class Product {
+  constructor(
+    public sku: string,
+    public name: string,
+    public imageUrl: string,
+    public department: string[],
+    public price: number
+  ) {
+
+  }
+}
