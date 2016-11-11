2142dadc82aa12698ec8339336987fe95d2e75c8
SergSlon <liamin.web@gmail.com>
2016-11-11 22:51:25 +0200

    rename app to inventory-app

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,2 +1,2 @@
-export * from './app.component';
-export * from './app.module';
+export * from './inventory-app.component';
+export * from './inventory-app.module';
--- src/app/inventory-app.component.ts
+++ src/app/inventory-app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'inventory-app.html',
+  styleUrls: ['inventory-app.component.css']
 })
 export class AppComponent {
   title = 'app works!';
